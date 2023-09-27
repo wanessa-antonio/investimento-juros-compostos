1df0f41435aeae1ac1bb48fe64b4799368552565
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 20:22:45 -0300

    commit add. 2

--- investimento-juros/src/Investimento.java
+++ investimento-juros/src/Investimento.java
@@ -1,6 +1,5 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
-
 public class Investimento {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,9 +9,13 @@ public class Investimento {
         System.out.print("Digite o valor inicial de investimento: ");
         double valorInicial = scanner.nextDouble();
 
+        System.out.println();
+
         // Solicita ao usuário que insira a taxa de juros anual
         System.out.print("Digite a taxa de juros anual (em decimal): ");
         double taxaJuros = scanner.nextDouble();
+
+        System.out.println();
 
         // Solicita ao usuário que insira a quantidade de anos de investimento
         System.out.print("Digite a quantidade de anos de investimento: ");
@@ -22,6 +25,8 @@ public class Investimento {
         for (int i = 0; i < periodo; i++) {
             valorFinal += valorFinal * taxaJuros;
         }
+
+        System.out.println();
 
         System.out.println("Valor final do investimento: R$ " + df.format(valorFinal));
 
